046e7d6383b0a23a5f9047181ff04676b0647b45
karcikovas <viktoras.karcikovas@gmail.com>
2019-07-22 01:26:36 +0300

    working on blog controller

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,11 +3,14 @@
 
 namespace App\Controller;
 
+use App\Entity\BlogPost;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * @Route("/blog")
@@ -34,7 +37,7 @@ class BlogController extends AbstractController
     ];
 
     /**
-     * @Route("/{page}", name="blog_list", defaults={"page":5})
+     * @Route("/{page}", name="blog_list", defaults={"page":5}, requirements={"page"="\d+"} )
      */
         public function list($page, Request $request)
         {
@@ -73,9 +76,18 @@ class BlogController extends AbstractController
     /**
      * @Route("/add", name="blog_add", methods={"POST"})
      */
-    public function add()
+    public function add(Request $request)
     {
+        /** @var Serializer $serializer */
+        $serializer = $this->get('serializer');
 
+        $blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, 'json');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($blogPost);
+        $em->flush();
+
+        return $this->json($blogPost);
     }
 
     /**
