017cc20870166a9b9cbd509059ea06c9b78054a6
karcikovas <viktoras.karcikovas@gmail.com>
2019-07-30 06:56:09 +0300

    added new migration

--- src/Migrations/Version20190730033349.php
+++ src/Migrations/Version20190730033349.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190727182454 extends AbstractMigration
+final class Version20190730033349 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -21,13 +21,13 @@ final class Version20190727182454 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE blog_post (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, published DATETIME NOT NULL, content LONGTEXT NOT NULL, author VARCHAR(255) NOT NULL, slug VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE blog_post CHANGE slug slug VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE blog_post');
+        $this->addSql('ALTER TABLE blog_post CHANGE slug slug VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci');
     }
 }
